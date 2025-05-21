c1eb9ebd4a947c794db0b6e49e80a1d7269aa991
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-21 01:22:03 -0300

    Hice que la página sea responsive

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function agregarTareas(e) {
     btnEliminar.appendChild(icono);
     if (tarea.trim() != "") {
         li.textContent = tarea;
-        li.classList.add("list-group-item", "w-100", "d-flex", "justify-content-between", "align-items-center");
+        li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
         lista.appendChild(li);
         icono.classList.add('bi', 'bi-trash');
         li.appendChild(btnEliminar);
